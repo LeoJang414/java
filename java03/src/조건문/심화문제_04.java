b21a4389eb992085f7d0b376f92a7622652b5f42
LeoJang414 <124111029+LeoJang414@users.noreply.github.com>
2023-02-08 23:18:13 +0900

    java03 프로젝트

---
+++ java03/src/조건문/심화문제_04.java
@@ -0,0 +1,28 @@
+package 조건문;
+
+import java.util.Scanner;
+
+public class 심화문제_04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("시작값 입력");
+		int num1 = sc.nextInt();
+		System.out.print("종료값 입력");
+		int num2 = sc.nextInt();
+		System.out.print("점프값 입력");
+		int num3 = sc.nextInt();
+
+		int sum = 0;
+		for (int i = num1; i <= num2; i+=num3) {
+			sum = sum + i;
+			if (sum > 100) {
+				break;
+			}
+		}
+		System.out.println(sum);
+				
+		sc.close();
+	}
+
+}
